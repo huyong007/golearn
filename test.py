b6855b191c0b4d69345320114e6aefdff4e210a7
胡永 <huyong@luochen.com>
2020-11-01 22:49:48 +0800

    继承

--- test.py
+++ test.py
@@ -1,6 +1,38 @@
-import learn
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+    
+    
 
-learn.test()
-print('123')
-learn.greeting('huyong')
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
 
+    def eat(self):
+        print('Eating meat...')
+
+
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+
+
+
+dog = Dog()
+
+dog.run()
+
+cat = Cat()
+
+cat.run()
+
+a = list()
+b = Animal()
+c = Dog()
+
+print(isinstance(a,list))
+
+print(isinstance(b,Animal))
+
+print(isinstance(c,Dog))
