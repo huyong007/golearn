01bac213a4ecbd90d8e150393fc85bf0bef4ace5
胡永 <1341696804@qq.com>
2020-11-17 14:34:57 +0800

    对ship类进行移动速度的改造

---
+++ air_fight/game_funcs.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+
+
+def check_events(ship):
+    '''响应按键和鼠标事件'''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            # 向右移动飞船
+            ship.moving_right = True
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            # 向左移动飞船
+            ship.moving_left = True
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            # 向上移动飞船
+            ship.moving_up = True
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            # 向下移动飞船
+            ship.moving_down = True
+        elif event.type == pygame.KEYUP:
+            ship.moving_up = ship.moving_down = ship.moving_left = ship.moving_right = False
+
+
+def update_screen(ai_setting, screen, ship):
+    '''更新屏幕上的图像,并切换到新屏幕'''
+    # 每次循环时都重绘屏幕
+    screen.fill(ai_setting.bg_color)
+    ship.blitme()
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
--- air_fight/ship.py
+++ air_fight/ship.py
@@ -2,9 +2,10 @@ import pygame
 
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         '''初始化飞船并设置其初始位置'''
         self.screen = screen
+        self.ai_settings = ai_settings
         # 记载飞船图片并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -12,6 +13,23 @@ class Ship():
         # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_recct.centerx
         self.rect.bottom = self.screen_recct.bottom
+        self.center = float(self.rect.centerx)
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        '''根据移动标志调整飞船的未知'''
+        if self.moving_right:
+            self.rect.centerx += 1
+        elif self.moving_left:
+            self.rect.centerx -= 1
+        elif self.moving_up:
+            self.rect.centery -= 1
+        elif self.moving_down:
+            self.rect.centery += 1
 
     def blitme(self):
         '''在指定未知绘制飞船'''
