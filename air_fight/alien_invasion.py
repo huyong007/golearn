903a31d17cbf00c622521ea1eed689e32b701911
胡永 <1341696804@qq.com>
2020-11-19 10:46:17 +0800

    发射子弹

--- air_fight/alien_invasion.py
+++ air_fight/alien_invasion.py
@@ -1,8 +1,10 @@
+
 import sys
 import pygame
 from settings import Settings
 from ship import Ship
 import game_funcs as gf
+from pygame.sprite import Group
 
 
 def run_game():
@@ -14,12 +16,15 @@ def run_game():
     pygame.display.set_caption("Air Fighting")
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
