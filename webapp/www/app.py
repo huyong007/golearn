46f1de82fb00be806d7bc87a119863a293d4f014
胡永 <1341696804@qq.com>
2021-01-12 11:01:19 +0800

    fix bug

--- webapp/www/app.py
+++ webapp/www/app.py
@@ -95,7 +95,7 @@ async def response_factory(app, handler):
                 return resp
         if isinstance(r, int) and r >= 100 and r < 600:
             return web.Response(r)
-        if isinstance(r, tuple) and len(r) = 2:
+        if isinstance(r, tuple) and len(r) == 2:
             t, m = r
             if isinstance(t, int) and t >= 100 and t < 600:
                 return web.Response(t, str(m))
